5adee343beb505c694b372324c8d5aeff4aa5649
Oleksii Hlavatskyi <qqlexa@flyaps.com>
2023-01-14 17:19:23 +0200

    Add languages logic

--- labs/models.py
+++ labs/models.py
@@ -3,24 +3,35 @@ from django.db import models
 
 class Laboratory(models.Model):
     title = models.CharField(max_length=20)
-    description = models.CharField(max_length=500)
-    pub_date = models.DateTimeField('date published')
+    # description = models.CharField(max_length=500)
+    # pub_date = models.DateTimeField('date published')
 
-    def __str__(self):
-        return f'{self.title}\n\n{self.description}'
+
+class Language(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=30)  # Title that user see
+
+
+class LaboratoryInLanguage(models.Model):
+    laboratory = models.ForeignKey(Laboratory, on_delete=models.CASCADE)  # 1 laboratory -> * laboratory in language
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)  # 1 language -> * laboratory in language
+
+    code = models.CharField(max_length=200)  # code that runs before user code
 
 
 class TestCase(models.Model):
     laboratory = models.ForeignKey(Laboratory, on_delete=models.CASCADE)  # 1 laboratory -> * laboratory tests
 
     description = models.CharField(max_length=200)  # description about this test
-    case_input = models.CharField(max_length=1000)  # input from system
-    case_output = models.CharField(max_length=1000)  # expected student output
+    description_input = models.CharField(max_length=1000)  # example input
+    description_output = models.CharField(max_length=1000)  # example output
+    expected_output = models.CharField(max_length=1000)  # expected student output
 
 
-class UserAnswer(models.Model):
-    laboratory = models.ForeignKey(Laboratory, on_delete=models.CASCADE)  # 1 laboratory -> * user answers
-    # TODO: user = models.ForeignKey(User, on_delete=models.CASCADE)  # user laboratory -> * user answers
-
-    code = models.CharField(max_length=5000)
-    upload_date = models.DateTimeField('date uploaded')
+# class UserAnswer(models.Model):
+#     laboratory = models.ForeignKey(Laboratory, on_delete=models.CASCADE)  # 1 laboratory -> * user answers
+#     language = models.ForeignKey(Language, on_delete=models.CASCADE)  # 1 language -> * user answers
+#     # TODO: user = models.ForeignKey(User, on_delete=models.CASCADE)  # user laboratory -> * user answers
+#
+#     code = models.CharField(max_length=5000)
+#     upload_date = models.DateTimeField('date uploaded')
--- labs/views.py
+++ labs/views.py
@@ -6,20 +6,26 @@ from django.http import HttpResponse, Http404
 from django.shortcuts import get_object_or_404, render
 import piston_rspy
 
-from .models import Laboratory, TestCase
+from .models import Laboratory, Language, LaboratoryInLanguage, TestCase
 
 def index(request):
-    latest_labs_list = Laboratory.objects.order_by('-pub_date')[:5]
+    latest_labs_list = Laboratory.objects.all()
     context = {'latest_labs_list': latest_labs_list}
     return render(request, 'index.html', context)
 
 async def detail(request, laboratory_id):
     laboratory = await get_laboratory(laboratory_id)
     test_cases = await get_laboratory_test_cases(laboratory)
+    lab_in_languages = await get_laboratory_available_languages(laboratory)
+    languages = await get_languages(lab_in_languages)
+
     if request.method == 'GET':
-        return render(request, 'detail.html', {'laboratory': laboratory, 'test_cases': test_cases})
+        return render(
+            request, 'detail.html',
+            {'laboratory': laboratory, 'test_cases': test_cases, 'languages': languages}
+        )
     else:
-        return await send(request, laboratory, test_cases)
+        return await send(request, laboratory, test_cases, lab_in_languages, languages)
 
 @sync_to_async
 def get_laboratory(laboratory_id: int) -> Laboratory:
@@ -31,44 +37,91 @@ def get_laboratory_test_cases(laboratory: Laboratory) -> list[TestCase]:
     return list(laboratory.testcase_set.all())
 
 
-async def send(request, laboratory, test_cases):
+@sync_to_async
+def get_laboratory_test_cases(laboratory: Laboratory) -> list[TestCase]:
+    return list(laboratory.testcase_set.all())
+
+
+@sync_to_async
+def get_laboratory_available_languages(laboratory: Laboratory) -> list[LaboratoryInLanguage]:
+    return list(laboratory.laboratoryinlanguage_set.all())
+
+
+@sync_to_async
+def get_languages(laboratory_available_languages: list[LaboratoryInLanguage]) -> list[Language]:
+    return [lil.language for lil in laboratory_available_languages]
+
+
+@sync_to_async
+def get_language_from_laboratory_languages(
+        lab_in_languages: list[LaboratoryInLanguage], language_title: str
+) -> tuple[LaboratoryInLanguage, Language]:
+    lab_in_language = [lil for lil in lab_in_languages if lil.language.title == language_title]
+    if len(lab_in_language) == 0:
+        raise Http404('Language cannot be empty.')
+    elif len(lab_in_language) > 1:
+        raise Http404('Language should be only one.')
+    lab_in_language = lab_in_language[0]
+    language = lab_in_language.language
+    return lab_in_language, language
+
+
+async def send(request, laboratory, test_cases, lab_in_languages, languages):
     user_code = request.POST.get('code', None)
     if user_code is None:
         raise Http404('User code cannot be empty.')
 
-    result = {}
+    language_title = request.POST.get('language', None)
+    if language_title is None:
+        raise Http404('Code language cannot be empty.')
+
+    lab_in_language, language = await get_language_from_laboratory_languages(
+        lab_in_languages, language_title
+    )
+
     input_coroutines = []
     for i, test_case in enumerate(test_cases):
-        file_content = f'{test_case.case_input}\n\n{user_code}'
-        # await
-        input_coroutines.append(get_file_output(i, file_content))
+        file_content = f'{lab_in_language.code}\n\n{user_code}'
+        input_coroutines.append(
+            get_file_output(i, file_content, language)
+        )
+
     print(datetime.datetime.now())
     system_cases = await asyncio.gather(*input_coroutines, return_exceptions=True)
     print(datetime.datetime.now())
 
     counter = 1
+    result = {}
     for test_case, system_output in zip(test_cases, system_cases):
-        result[f'Test {counter}'] = test_case.case_output == system_output
+        result[f'Test {counter}'] = test_case.expected_output == system_output
         counter += 1
 
-    return render(request, 'detail.html', {'laboratory': laboratory, 'result': result, 'test_cases': test_cases})
+    return render(
+        request, 'detail.html',
+        {
+            'laboratory': laboratory,
+            'result': result,
+            'test_cases': test_cases,
+            'languages': languages,
+            'lab_in_languages': lab_in_languages
+        }
+    )
 
 
-async def get_file_output(time, file_content):
+async def get_file_output(time, file_content: str, language: Language):
     await asyncio.sleep(0.3 * time)
-    file = piston_rspy.File(
-        name='main.py',
-        content=file_content,
-    )
-
-    executor = piston_rspy.Executor(
-        language='python',  # TODO: Add choice of languages
-        version='3.10',
-        files=[file],
-    )
 
     client = piston_rspy.Client()
-    response = await client.execute(executor)
+    response = await client.execute(
+        piston_rspy.Executor()
+        .set_language(language.title)
+        .add_file(
+            piston_rspy.File(
+                name="main",
+                content=file_content,
+            )
+        )
+    )
 
     user_output = response.run.output.removesuffix('\n')
     print(f'Language: {response.language} v{response.version}')
